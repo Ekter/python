c63fd2c20f3934bdde397e313e5a4ae43a6459f6
Ekter Moi <ektermoi1@gmail.com>
2022-10-04 22:19:20 +0200

    transparent

--- test2.py
+++ test2.py
@@ -9,24 +9,7 @@ import numpy as np
 base = ShowBase()
 base.disableMouse()
 base.camera.setPos(0, -10, 0)
-def makeCube(base,dimension,position,color_array_ugly=[[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]]):
-    # title = OnscreenText(text="Panda3D: Tutorial - Making a Cube Procedurally",
-    #                      style=1, fg=(1, 1, 1, 1), pos=(-0.1, 0.1), scale=.07,
-    #                      parent=base.a2dBottomRight, align=TextNode.ARight)
-    # escapeEvent = OnscreenText(text="1: Set a Texture onto the Cube",
-    #                            style=1, fg=(1, 1, 1, 1), pos=(0.06, -0.08),
-    #                            align=TextNode.ALeft, scale=.05,
-    #                            parent=base.a2dTopLeft)
-    # spaceEvent = OnscreenText(text="2: Toggle Light from the front On/Off",
-    #                           style=1, fg=(1, 1, 1, 1), pos=(0.06, -0.14),
-    #                           align=TextNode.ALeft, scale=.05,
-    #                           parent=base.a2dTopLeft)
-    # upDownEvent = OnscreenText(text="3: Toggle Light from on top On/Off",
-    #                            style=1, fg=(1, 1, 1, 1), pos=(0.06, -0.20),
-    #                            align=TextNode.ALeft, scale=.05,
-    #                            parent=base.a2dTopLeft)
-
-
+def makeCube(base,dimension,position,color_array_ugly=[[1,0,0,0.5],[1,0,0,0.5],[1,0,0,0.5],[1,0,0,0.5]],trans=False):
     # You can't normalize inline so this is a helper function
     def normalized(*args):
         myVec = LVector3(*args)
@@ -72,25 +55,21 @@ def makeCube(base,dimension,position,color_array_ugly=[[1,0,0,0],[0,1,0,0],[0,0,
         # adding different colors to the vertex for visibility
         [color.addData4f(*l) for l in color_array_ugly]
 
-        # texcoord.addData2f(0.0, 1.0)
-        # texcoord.addData2f(0.0, 0.0)
-        # texcoord.addData2f(1.0, 0.0)
-        # texcoord.addData2f(1.0, 1.0)
-
         # Quads aren't directly supported by the Geom interface
         # you might be interested in the CardMaker class if you are
         # interested in rectangle though
-        tris = GeomTriangles(Geom.UHDynamic)
+        tris = GeomTriangles(Geom.UHDynamic)#Geom.UHDynamic)UHStatic
         tris.addVertices(0, 1, 3)
         tris.addVertices(1, 2, 3)
 
         square = Geom(vdata)
         square.addPrimitive(tris)
+        # square.setDepthWrite(1)
         return square
 
 
     # Note: it isn't particularly efficient to make every face as a separate Geom.
-    # instead, it would be better to create one Geom holding all of the faces.
+    # instead, it would be better to create one Geom holding all of the faces. TODO change this
     square0 = makeSquare(position[0], position[1], position[2], position[0]+dimension[0], position[1], position[2]+dimension[2])
     square1 = makeSquare(position[0], position[1]+dimension[1], position[2], position[0]+dimension[0], position[1]+dimension[1], position[2]+dimension[2])
     square2 = makeSquare(position[0], position[1]+dimension[1], position[2]+dimension[2], position[0]+dimension[0], position[1], position[2]+dimension[2])
@@ -111,57 +90,11 @@ def makeCube(base,dimension,position,color_array_ugly=[[1,0,0,0],[0,1,0,0],[0,0,
     # OpenGl by default only draws "front faces" (polygons whose vertices are
     # specified CCW).
     cube.setTwoSided(True)
+    cube.setTransparency(True)
+    if trans:
+        cube.setRenderModeWireframe()
 
-
-    class MyTapper(DirectObject):
-
-        def __init__(self):
-            pass
-            # self.testTexture = loader.loadTexture("maps/envir-reeds.png")
-            # self.accept("1", self.toggleTex)
-            # self.accept("2", self.toggleLightsSide)
-            # self.accept("3", self.toggleLightsUp)
-
-            # self.LightsOn = False
-            # self.LightsOn1 = False
-            # slight = Spotlight('slight')
-            # slight.setColor((1, 1, 1, 1))
-            # lens = PerspectiveLens()
-            # slight.setLens(lens)
-            # self.slnp = render.attachNewNode(slight)
-            # self.slnp1 = render.attachNewNode(slight)
-
-        # def toggleTex(self):
-        #     global cube
-        #     if cube.hasTexture():
-        #         cube.setTextureOff(1)
-        #     else:
-        #         cube.setTexture(self.testTexture)
-
-        # def toggleLightsSide(self):
-        #     global cube
-        #     self.LightsOn = not self.LightsOn
-
-        #     if self.LightsOn:
-        #         render.setLight(self.slnp)
-        #         self.slnp.setPos(cube, 10, -400, 0)
-        #         self.slnp.lookAt(10, 0, 0)
-        #     else:
-        #         render.setLightOff(self.slnp)
-
-        # def toggleLightsUp(self):
-        #     global cube
-        #     self.LightsOn1 = not self.LightsOn1
-
-        #     if self.LightsOn1:
-        #         render.setLight(self.slnp1)
-        #         self.slnp1.setPos(cube, 10, 0, 400)
-        #         self.slnp1.lookAt(10, 0, 0)
-        #     else:
-        #         render.setLightOff(self.slnp1)
-
-
-    t = MyTapper()
+    t = DirectObject()
     base.run()
 
-makeCube(base, np.array([3, 3, 3]), np.array([-1.5, -1.5, -1.5]))
+makeCube(base, np.array([3, 3, 3]), np.array([0,0,0]),False)
