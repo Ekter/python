2f7c88d6380d3c227b4a14e9061d164c01f86b81
Ekter <ektermoi1@gmail.com>
2023-03-09 18:56:34 +0100

    errtfghj

--- touches/ring_jimmy.py
+++ touches/ring_jimmy.py
@@ -6,27 +6,19 @@ duration = 1000  # Set Duration To 1000 ms == 1 second
 
 
 def on_press(key):
-    ct = keyboard.Controller()
     # C'EST TROP PUISSANT PUTAIN
     try:
         print(f"alphanumeric key {key.char} pressed")
         if key.char == "a":
             winsound.Beep(frequency, duration)
 
-            ct.press(keyboard.Key.backspace)
-            ct.release(keyboard.Key.backspace)
-            ct.press(keyboard.Key.backspace)
-            ct.release(keyboard.Key.backspace)
-            ct.type("newline")
-            ct.press(keyboard.Key.enter)
-            ct.release(keyboard.Key.enter)
     except AttributeError:
         print("special key {0} pressed".format(key))
 
 
 def on_release(key):
     print("{0} released".format(key))
-    if key == keyboard.Key.end:
+    if key.char == "q":
         # Stop listener
         print("closed listner")
         return False
