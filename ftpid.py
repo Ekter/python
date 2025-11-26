605b8422e5a78889a35b1abb12fdd1335f92dcf1
Ekter <ektermoi1@gmail.com>
2025-11-26 02:01:56 +0100

    feat: tuned pid controller for variable input

--- ftpid.py
+++ ftpid.py
@@ -1,5 +1,8 @@
 import ftprci as fci
+
+# import time
 import matplotlib.pyplot as plt
+import math
 
 
 class System:
@@ -8,7 +11,7 @@ class System:
         self.C = 3
         self.Tmes = 18.0
         self.Text = 12.0
-        self.Pconst = 2000.0
+        self.Pconst = -30.0
 
     def command(self, pchauf: float):
         self.Tmes = (
@@ -25,34 +28,45 @@ th_system = System()
 
 pid = fci.PIDController(3, 3, 0, fci.DiscreteIntegral.EulerF(dt=1))
 
-fig = plt.figure(0)
-fig2 = plt.figure(1)
+fig0 = plt.figure(0)
+fig1 = plt.figure(1)
+fig2 = plt.figure(2)
 
-ax1 = fig.add_subplot()
-ax1.set_title("Temperature Control")
+ax0 = fig0.add_subplot()
+ax0.set_title("Temperature actuelle")
+ax0.set_xlabel("Time (s)")
+ax0.set_ylabel("Temperature (°C)")
+
+ax1 = fig1.add_subplot()
+ax1.set_title("Consigne")
 ax1.set_xlabel("Time (s)")
-ax1.set_ylabel("Temperature (°C)")
+ax1.set_ylabel("Control Signal")
 
 ax2 = fig2.add_subplot()
+ax2.set_title("Temperature souhaitee")
 ax2.set_xlabel("Time (s)")
-ax2.set_ylabel("Control Signal")
+ax2.set_ylabel("Temperature (°C)")
 
-
-c = fci.Clock(1/100, 2)
+c = fci.Clock(1 / 100, 2)
 p = fci.ProgressBar(c)
 
-log = fci.logger.ClockedPlotLogger1D(c, [ax1], update_freq=10000)
-log2 = fci.logger.ClockedPlotLogger1D(c, [ax2], update_freq=10000)
+log = fci.logger.ClockedPlotLogger1D(c, [ax0], update_freq=10000)
+log2 = fci.logger.ClockedPlotLogger1D(c, [ax1], update_freq=10000)
+log3 = fci.logger.ClockedPlotLogger1D(c, [ax2], update_freq=10000)
 
 
 pid.set_order(21)
 
-c >> p | th_system.read | log | pid | log2 | th_system.command
+new_order = lambda t: (pid.set_order(18 + 5 * math.cos(2 * math.pi * t / 2)), t, pid.order)[2]
+
+c >> p | new_order | log3 | th_system.read | log | pid | log2 | th_system.command
 c.start()
+
 
 c.wait()
 log.plot()
 log2.plot()
+log3.plot()
 # log.
 plt.show()
 
